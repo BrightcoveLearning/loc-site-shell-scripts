db746d413bbff7e01f4305fa1b5e707c867f07e2
Robert <rcrooks@brightcove.com>
2021-09-18 11:13:13 -0400

    wip

--- convert2utf8.sh
+++ convert2utf8.sh
@@ -1,4 +1,5 @@
-FILES=*.html
+sudo chmod -R g+w /Users/rcrooks/github
+FILES=find /Users/rcrooks/GitHub/Japanese -name '*.html'
 for f in $FILES
 do
   filename="${f%.*}"
@@ -9,9 +10,11 @@ do
     mkdir -p converted
     cp $f ./converted
     iconv -f ISO-8859-1 -t UTF-8 $f > "${filename}_utf8.html"
-    mv "${filename}_utf8.html" $f
+    mv "${filename}.html" $f
     echo ": CONVERTED TO UTF-8."
   else
     echo ": UTF-8 ALREADY."
   fi
 done
+
+find . -name '*.html' -exec iconv --verbose -f ISO-8859-1 -t utf-8 {} {} \;
